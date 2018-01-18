d7a07cea9c988d350a654f98b28f26d1acc139dd
IvgNarinsky <33311257+IvgNarinsky@users.noreply.github.com>
2018-01-18 09:57:11 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+   $(function(){//main
+          $(window).keypress(function(e){
+		   if(e.keyCode === 13)
+		    {
+            newItem();
+			}
+			});
+			function newItem()
+			{
+			var item=$('#todo').val();
+			$('#todos').append('<li>'+item+'</li>');
+            $('#todo').val('');			
+			}
+			$('ul').on('click','li',function(e) {
+			  $(e.target).remove();
+			});
+			
+            });
